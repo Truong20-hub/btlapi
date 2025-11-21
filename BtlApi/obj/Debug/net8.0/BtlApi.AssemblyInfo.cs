d5d929276d455c01ce8edba3b8528711e473bbcf
= <td9358525@gmail.com>
2025-11-22 01:42:33 +0700

    Apply gitignore properly

--- BtlApi/obj/Debug/net8.0/BtlApi.AssemblyInfo.cs
+++ BtlApi/obj/Debug/net8.0/BtlApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("btlapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec34fd9fea2752044b554a4ab5193f5fabf3f23")]
 [assembly: System.Reflection.AssemblyProductAttribute("btlapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("btlapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
